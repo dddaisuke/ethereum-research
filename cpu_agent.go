2fdc047a7147c20b59947897d7edeb9e59f7370a
dddaisuke <dddaisuke@gmail.com>
2017-10-23 17:01:56 +0900

    Create cpu_agent.go

---
+++ cpu_agent.go
@@ -0,0 +1,13 @@
+type CpuAgent struct {
+	mu sync.Mutex
+
+	workCh        chan *Work
+	stop          chan struct{}
+	quitCurrentOp chan struct{}
+	returnCh      chan<- *Result
+
+	chain  consensus.ChainReader
+	engine consensus.Engine
+
+	isMining int32 // isMining indicates whether the agent is currently mining
+}
